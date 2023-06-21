93f7d2857bc98a6f3dd435ad67faffb658d82cc5
Anuj Panwar <anujpanwar7911@gmail.com>
2023-06-21 16:32:02 +0000

    learned how to send the html content as response.

--- tutorial/create-node-server/app.js
+++ tutorial/create-node-server/app.js
@@ -1,6 +1,15 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req);
+  // console.log(req.url,req.method,req.headers);
+  // checked the url, method =[Get, post], headers = some browser headers
+
+  res.setHeader("Content-type", "text/html");
+  res.write("<html>")
+  res.write("<head><title>Anuj Panwar here </title></head>")
+  res.write("<body><h1>Hello Anuj Panwar ka project hai ye</h1></body>")
+  res.write("</html>")
+  res.end(); // telling the node our response is end here
+
 });
 
 server.listen(3000);
